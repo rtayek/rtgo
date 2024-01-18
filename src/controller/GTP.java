35cddff00928ad85bcb6a41663ecd21408eeef64
unknown <rtayek@gmail.com>
2024-01-18 00:36:46 -0800

    added a comment.

--- src/controller/GTP.java
+++ src/controller/GTP.java
@@ -1,6 +1,7 @@
 package controller;
 import io.IO.Stopable;
 interface GTPx extends Stopable {
+	// does not appear to be used at present.
     boolean messageSansLinefeed(Character character,Integer id,String string);
     boolean message(Character character,int id,String string);
     boolean isWaitingForMove();
